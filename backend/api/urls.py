c5424e640746af3fc0633473fb53c303d1c53a29
Imad Dabligi <imad.dabligi@gmail.com>
2024-11-02 12:02:44 +0100

    refactor(Backend/settings): Move api routing from backend/urls to api/urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import CustomTokenObtainPairView, CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    # path("notes/", views.NoteListCreate.as_view(), name="note-list"),
-    # path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
+    path("user/register/", CreateUserView.as_view(), name="register"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="get_token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
